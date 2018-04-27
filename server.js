ab4938ee88784cc9f4ac15e6cb51939b7b1dcd54
Joshua Dulin <jzdulin@wifi_stu-10-38-88-84.uncc.edu>
2018-04-27 13:57:42 -0400

    set up the server

---
+++ server.js
@@ -0,0 +1,47 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var request = require("request");
+var mongoose = require("mongoose");
+var path = require("path");
+var logger = require("morgan");
+
+
+// Require all models!!
+//commented out until we actually have models
+// var db = require("./models");
+
+var PORT = process.env.PORT || 3000;
+
+//use express
+var app = express();
+
+//Dont think we need this right now but its here for later!
+// app.use(logger("dev"));
+
+//use body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+//Get those routes when we have them!
+// require("./controller/user-routes.js")(app);
+// require("./controller/subscription-routes.js")(app);
+// require("./controller/html-routes.js")(app);
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+//move to routes eventually
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
+  
