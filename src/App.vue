f14337759f3a5a58deb7d0bd6f82d7a12034b10c
Klara Boyer <kawboyer@gmail.com>
2018-04-29 17:41:46 -0400

    testing adding single-file components created in Vue.js to the exisiting file structure

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<!--App.vue (Parent component)-->
+<template>
+  <div id="app">
+    <navbar-component></navbar-component>
+    <div class="container">
+      <page-heading-component></page-heading-component>
+      <staff-list-component></staff-list-component>
+      <subscription></subscription>
+    </div>
+  </div>
+</template>
+
+<script>
+  import PageHeadingComponent from './components/PageHeadingComponent.vue'
+  import StaffListComponent from './components/StaffListComponent.vue'
+  import NavbarComponent from './components/NavbarComponent.vue'
+  import Subscription from './components/Subscription.vue'
+  export default {
+    name: 'app',
+    components: {NavbarComponent, PageHeadingComponent, StaffListComponent, Subscription}, //Register other components
+  }
+</script>
+
+<style scoped>
+</style>
