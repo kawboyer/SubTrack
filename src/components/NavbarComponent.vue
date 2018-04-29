25f77b3a9edd4489994fbaa9c4b7fbd77db60855
Klara Boyer <kawboyer@gmail.com>
2018-04-29 19:01:27 -0400

    changes to App.vue import

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,20 +1,20 @@
 <!--NavbarComponent.vue-->
 <template>
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-        <a class="navbar-brand" href="#">ACME INTL</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
-                aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-    </nav>
+	<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+			<a class="navbar-brand" href="#">ACME INTL</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+				aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+			</button>
+	</nav>
 </template>
 
 <script>
-    module.exports = {
-        data(){
-            return {}
-        }
-    }
+module.exports = {
+  data() {
+    return {};
+  }
+};
 </script>
 
 
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 new Vue({
-  el: 'app',
+  el: '#app',
   components:{App}
 })
 
